09748eb57e49419c2f66dd315e367e10781ad476
Studio Velysion <studiovelygame@gmail.com>
2026-01-06 23:43:38 +0100

    chore: vider le depot

--- lib/theme/velysion-chakra-theme.ts
+++
@@ -1,7 +0,0 @@
-import { defaultSystem } from '@chakra-ui/react'
-
-// Dans Chakra UI v3, on utilise defaultSystem directement
-// Les couleurs personnalisées sont appliquées via CSS variables dans ClientChakraProvider
-const theme = defaultSystem
-
-export default theme
--- types/notes.ts
+++
@@ -1,9 +0,0 @@
-export interface QuickNote {
-  id: string
-  userId: string
-  content: string
-  tag: string | null
-  createdAt: string
-  updatedAt: string
-}
-
